bab24ae19f6aec50f036f1c6868a00fa015b0267
Colin Jones <trptcolin@gmail.com>
2015-06-02 21:55:51 -0500

    Extract a test function for later use
    Also improves reporting by including the test name in both failures and
    passing tests. Not everything should be this high-level of a test, and
    eventually I'll want to get ahold of a C unit testing setup, but for now
    it feels OK to hack around learning the language and APIs with a sanity
    check or two making sure the happy path still works.
    
    This extracted test function will be helpful for IPv6 addresses. I'm
    currently not able to support those due to inet_ntoa and inet_aton,
    which it turns out are both considered obsolete since they only work
    with IPv4.
    
    So coming up soon will be cutting over to getaddrinfo / getnameinfo for
    going back & forth between strings & sockaddrs.

--- test/echo_test.sh
+++ test/echo_test.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-test_server_host="127.0.0.1"
-test_server_port=12345
-
 function cleanup_subprocesses() {
   for pid in $@; do
     kill $pid 2>/dev/null
@@ -15,29 +12,43 @@ export -f cleanup_subprocesses
 trap 'cleanup_subprocesses $(jobs -p)' EXIT;
 
 function wait_for_server_connection() {
-  max_seconds_to_wait_for_server_connection=$1
+  test_server_host=$1
+  test_server_port=$2
+  max_seconds_to_wait_for_server_connection=$3
 
   i=0
   while ! nc $test_server_host $test_server_port </dev/null; do
     sleep 1
     ((i++)) || true
     if [ $i -gt $max_seconds_to_wait_for_server_connection ]; then
-      echo
-      echo "[FAIL] ** Server connection timed out"
-      exit 1
+      return 1
     fi
   done
+  return 0
 }
 
-build/echo $test_server_host $test_server_port 2>&1 >/dev/null &
+max_wait_seconds=3
 
-wait_for_server_connection 3
+function test_successful_echo() {
+  test_name=$1
+  test_server_host=$2
+  test_server_port=$3
+  build/echo $test_server_host $test_server_port 2>&1 >/dev/null &
 
-expected_output=$(echo -e "ohai\nkthxbai")
-actual_output=$(echo -e "ohai\nkthxbai" | nc $test_server_host $test_server_port)
+  if ! wait_for_server_connection $test_server_host $test_server_port $max_wait_seconds; then
+    echo "[FAIL][Test \"$test_name\"] server startup timed out"
+    return 1
+  fi
 
-if [ "$expected_output" == "$actual_output" ]; then
-  echo Pass
-else
-  echo "[FAIL] expected '$expected_output', but got '$actual_output'"
-fi
+  expected_output=$(echo -e "ohai\nkthxbai")
+  actual_output=$(echo -e "ohai\nkthxbai" | nc $test_server_host $test_server_port)
+
+  if [ "$expected_output" == "$actual_output" ]; then
+    echo "Test \"$test_name\" passed"
+  else
+    echo "[FAIL][Test \"$test_name\"] expected '$expected_output', but got '$actual_output'"
+  fi
+}
+
+test_successful_echo "ipv4 address" 127.0.0.1 4567
+#test_successful_echo "ipv6 address" ::1 4567
