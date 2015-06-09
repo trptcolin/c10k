062ec982a88d864c84b5c4fc5c5549b79faf94a8
Colin Jones <trptcolin@gmail.com>
2015-06-09 06:52:18 -0500

    Extract function to bind socket given criteria
    This didn't present any problems - just extracting, renaming, limiting
    to 80 characters per line. But in extracting functions, I'm realizing
    that the discipline of deciding who allocates memory is really useful: I
    definitely don't want to have to track over a whole program what memory
    is out there needing to be freed at some point. Much easier to have it
    within a small lexical scope (even if that scope wraps up the whole
    program), so you can see where it needs to be freed.
    
    Also removed all exit(3) calls in favor of normal exit - should make
    unit testing / reusability better when I get there.

--- src/echo.c
+++ src/echo.c
@@ -12,10 +12,12 @@ void handle_connections_forever(int socket) {
   while(1) {
     struct sockaddr accepted_socket_address;
     socklen_t accepted_socket_address_length = sizeof(struct sockaddr);
-    int accepted_fd = accept(socket, &accepted_socket_address, &accepted_socket_address_length);
+    int accepted_fd = accept(socket,
+                             &accepted_socket_address,
+                             &accepted_socket_address_length);
     if(accepted_fd < 0) {
       perror("accept() error");
-      exit(EXIT_FAILURE);
+      continue;
     }
 
     char input[1];
@@ -30,30 +32,75 @@ void handle_connections_forever(int socket) {
   }
 }
 
-void print_addrinfo_results_node(struct addrinfo* my_addrinfo_results_node) {
-  char my_host_result[NI_MAXHOST];
-  char my_port_result[NI_MAXSERV];
-  int my_getname_result = getnameinfo(my_addrinfo_results_node->ai_addr,
-      my_addrinfo_results_node->ai_addrlen,
-      my_host_result,
-      NI_MAXHOST,
-      my_port_result,
-      NI_MAXSERV,
-      NI_NUMERICHOST | NI_NUMERICSERV
-      );
+void print_addrinfo(struct addrinfo* addrinfo) {
+  char host[NI_MAXHOST];
+  char port[NI_MAXSERV];
+  int my_getname_result = getnameinfo(addrinfo->ai_addr,
+                                      addrinfo->ai_addrlen,
+                                      host,
+                                      NI_MAXHOST,
+                                      port,
+                                      NI_MAXSERV,
+                                      NI_NUMERICHOST | NI_NUMERICSERV);
   if(my_getname_result != 0) {
     perror("getnameinfo() error");
-    exit(EXIT_FAILURE);
+  } else {
+    printf("host/port: '%s/%s'\n", host, port);
   }
-  printf("host: '%s'\n", my_host_result);
-  printf("port: '%s'\n", my_port_result);
+}
+
+int bind_socket(const char* host,
+                const char* port,
+                struct addrinfo* hints) {
+  const int error = -1;
+
+  struct addrinfo* addrinfo_results_head;
+  int getaddrinfo_result = getaddrinfo(host,
+                                       port,
+                                       hints,
+                                       &addrinfo_results_head);
+  if(getaddrinfo_result != 0) {
+    const char* error_string = gai_strerror(getaddrinfo_result);
+    printf("Error parsing host/port: %s/%s: %s\n", host, port, error_string);
+    return error;
+  } else if (addrinfo_results_head == NULL) {
+    printf("No match found for host/port: %s/%s\n", host, port);
+    return error;
+  }
+
+  struct addrinfo* my_addrinfo_results_node;
+  int sock;
+  for(my_addrinfo_results_node = addrinfo_results_head;
+      my_addrinfo_results_node != NULL;
+      my_addrinfo_results_node = my_addrinfo_results_node->ai_next) {
+
+    sock = socket(my_addrinfo_results_node->ai_family,
+                  my_addrinfo_results_node->ai_socktype,
+                  my_addrinfo_results_node->ai_protocol);
+    if(sock < 0) {
+      perror("socket(2) error");
+      return error;
+    }
+
+    int bind_result = bind(sock,
+                           my_addrinfo_results_node->ai_addr,
+                           my_addrinfo_results_node->ai_addrlen);
+    if(bind_result < 0) {
+      perror("bind() error");
+      continue; // try another addrinfo result
+    } else {
+      print_addrinfo(my_addrinfo_results_node);
+      break; // found a good one
+    }
+  }
+
+  freeaddrinfo(addrinfo_results_head);
+  return sock;
 }
 
 int main(int argc, char* const argv[]) {
-  struct addrinfo* my_addrinfo_results_head_node;
-
-  char* requested_host;
-  char* requested_port;
+  char* requested_host = NULL;
+  char* requested_port = NULL;
 
   if(argc == 2) {
     requested_host = "0.0.0.0";
@@ -63,7 +110,7 @@ int main(int argc, char* const argv[]) {
     requested_port = argv[2];
   } else {
     printf("Usage: echo [HOST] PORT_NUMBER\n");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   struct addrinfo hints = {
@@ -72,43 +119,16 @@ int main(int argc, char* const argv[]) {
     .ai_flags = AI_PASSIVE,
   };
 
-  int getaddrinfo_result = getaddrinfo(requested_host, requested_port, &hints, &my_addrinfo_results_head_node);
-  if(getaddrinfo_result != 0) {
-    printf("Error parsing host/port: %s/%s: %s\n", requested_host, requested_port, gai_strerror(getaddrinfo_result));
-    exit(EXIT_FAILURE);
-  }
-
-  struct addrinfo* my_addrinfo_results_node;
-
-  int sock = 0;
-  for(my_addrinfo_results_node = my_addrinfo_results_head_node;
-      my_addrinfo_results_node != NULL;
-      my_addrinfo_results_node = my_addrinfo_results_node->ai_next) {
-    sock = socket(my_addrinfo_results_node->ai_family,
-                  my_addrinfo_results_node->ai_socktype,
-                  my_addrinfo_results_node->ai_protocol);
-
-    int bind_result = bind(sock, my_addrinfo_results_node->ai_addr, my_addrinfo_results_node->ai_addrlen);
-    if(bind_result < 0) {
-      perror("bind() error");
-      continue; // try another addrinfo result
-    } else {
-      print_addrinfo_results_node(my_addrinfo_results_node);
-      break; // found a good one
-    }
-  }
-
-  freeaddrinfo(my_addrinfo_results_head_node);
-
-  if(sock == 0) {
+  int sock = bind_socket(requested_host, requested_port, &hints);
+  if(sock < 0) {
     printf("socket wasn't bound\n");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   int listen_result = listen(sock, 1);
   if(listen_result < 0) {
     perror("listen() error");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   handle_connections_forever(sock);
